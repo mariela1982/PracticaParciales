068d9f8ce5ef9d162f4674f908370928acc4ac6c
mariela1982 <marimanri_24@hotmail.com>
2023-10-31 17:25:07 -0300

    faltoEsto

--- src/ar/edu/unlam/pb2/Tarjeta.java
+++ src/ar/edu/unlam/pb2/Tarjeta.java
@@ -1,5 +1,14 @@
 package ar.edu.unlam.pb2;
 
 public abstract class Tarjeta {
+	private Long numero;
+	private String titular;
+	private String fechaDeVencimiento;
+	private Integer codigoDeSeguridad;
+	
+	public Tarjeta(Long numero, String titular, String fechaDeVencimiento,
+			Integer codigoDeSeguridad) {
+		
+	}
 
 }
--- src/ar/edu/unlam/pb2/TarjetaDeCredito.java
+++ src/ar/edu/unlam/pb2/TarjetaDeCredito.java
@@ -1,5 +1,47 @@
 package ar.edu.unlam.pb2;
 
-public class TarjetaDeCredito {
+public class TarjetaDeCredito implements Pagadora {
+
+	@Override
+	public Boolean pagar(Persona vendedor, Double importe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSaldo(Double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long getNumero() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitular() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getCodigoDeSeguridad() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFechaDeVencimiento() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double getSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
